1f1ab23ee2154887c0dbdc81c68cb79461c34fc9
4118 Горожанкин А.А <rememberov.andrei@yandex.ru>
2023-03-22 00:14:20 +0300

    init project

---
+++ client/app/components/ui/Layout/Layout.tsx
@@ -0,0 +1,20 @@
+import { FC, PropsWithChildren } from 'react'
+
+import Meta from '@/utils/meta/Meta'
+import { IMeta } from '@/utils/meta/meta.interface'
+
+import Header from './header/Header'
+
+const Layout: FC<PropsWithChildren<IMeta>> = ({ children, ...meta }) => {
+	return (
+		<>
+			<Meta {...meta} />
+			<div>
+				<Header />
+				<main>{children}</main>
+			</div>
+		</>
+	)
+}
+
+export default Layout
